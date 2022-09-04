379a29f3ced81ab635d484079a79d6112686063b
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-04 13:02:40 +0200

    Data Seeder Script

---
+++ server/seeder.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const users = require('./data/users');
+const products = require('./data/products');
+const User = require('./models/User');
+const Product = require('./models/Product');
+const Order = require('./models/Order');
+const connectDB = require('./config/db');
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+    try {
+        await User.deleteMany();
+        await Product.deleteMany();
+        await Order.deleteMany();
+
+        const createdUser = await User.insertMany(users);
+
+        const adminUser = createdUser[0]._id;
+
+        const sampleProducts = products.map(product => {
+            return { ...product, user: adminUser };
+        });
+
+        await Product.insertMany(sampleProducts);
+
+        console.log('Data Imported!');
+        process.exit();
+    } catch (error) {
+        console.error(`${error}`);
+        process.exit(1);
+    }
+};
+
+const destroyData = async () => {
+    try {
+        await User.deleteMany();
+        await Product.deleteMany();
+        await Order.deleteMany();
+
+        console.log('Data Destroyed!');
+        process.exit();
+    } catch (error) {
+        console.error(`${error}`);
+        process.exit(1);
+    }
+};
+
+if (process.argv[2] === '-d') {
+    destroyData();
+} else {
+    importData();
+}
