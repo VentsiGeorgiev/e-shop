77953c54884812a2be7c98bc16398ba70f76ea2a
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-04 07:31:46 +0200

    Add single product router / controller

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -4,6 +4,12 @@ const getProducts = (req, res) => {
     res.json(products);
 };
 
+const getProduct = (req, res) => {
+    const product = products.find(p => p._id === req.params.id);
+    res.json(product);
+};
+
 module.exports = {
-    getProducts
+    getProducts,
+    getProduct
 };
