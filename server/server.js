3b1e8771d21060a8e7dd9afa679726068b187927
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-04 10:49:20 +0200

    Connect to the database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const cors = require('./middleware/cors');
 const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 const app = express();
 
