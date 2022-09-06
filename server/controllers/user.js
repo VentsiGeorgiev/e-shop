d4111ffe0295bd874ea1e937b809aaee1d6d147c
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-06 19:38:47 +0200

    Register User

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,62 @@
-const registerUser = (req, res) => {
-    res.send('Register user');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+// @desc   Register a new user
+// @route  /api/users
+// @access Public
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        // Validation
+        if (!name || !email || !password) {
+            res.status(400);
+            throw new Error('All fields are required');
+        }
+
+        // Find if user already exists
+        const userExists = await User.findOne({ email });
+
+        if (userExists) {
+            res.status(400);
+            throw new Error('User already exists');
+        }
+
+        // Hash password
+        const hashedPassword = bcrypt.hashSync(password, 10);
+
+        // Create user
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        if (user) {
+            res.status(201).json({
+                _id: user._id,
+                name: user.name,
+                email: user.email
+            });
+        } else {
+            res.status(400);
+            throw new Error('Invalid user data');
+        }
+
+
+    } catch (err) {
+        console.error(err);
+        res.status(404);
+        res.json({ message: err.message });
+    }
+
+
+
 };
 
+// @desc   Login user
+// @route  /api/users/login
+// @access Public
 const loginUser = (req, res) => {
     res.send('Login user');
 };
