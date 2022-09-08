c5a432dd20207d8e164121d401cb63aa349fe92f
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-09-08 08:34:42 +0200

    Chaning localStorage with user utils functions

--- client/src/features/auth/authService.js
+++ client/src/features/auth/authService.js
@@ -1,5 +1,5 @@
 import { request, createOptions } from '../../hooks/useFetch';
-import { getToken, setUserData } from '../../utils/user';
+import { clearUserData, setUserData } from '../../utils/user';
 const API_URL = '/api/users';
 
 // Register user
@@ -8,7 +8,7 @@ const register = async (userData) => {
     const result = await request(API_URL, createOptions('post', userData));
 
     if (result) {
-        localStorage.setItem('user', JSON.stringify(result));
+        setUserData(result);
     }
 
     return result;
@@ -21,7 +21,7 @@ const login = async (userData) => {
     const result = await request(`${API_URL}/login`, createOptions('post', userData));
 
     if (result) {
-        localStorage.setItem('user', JSON.stringify(result));
+        setUserData(result);
     }
 
     return result;
@@ -29,7 +29,7 @@ const login = async (userData) => {
 
 
 // Logout
-const logout = () => localStorage.removeItem('user');
+const logout = () => clearUserData('user');
 
 // Update user
 const update = async (user) => {
--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,4 +1,4 @@
-import { getToken } from "../utils/user";
+import { getToken } from '../utils/user';
 
 export async function request(url, options) {
 
