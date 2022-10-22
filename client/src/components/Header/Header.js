c034c919b1064943fa75e02fa8f1e90b29babef4
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-22 18:53:19 +0200

    Add styles for navbar;

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,6 +2,7 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout, reset } from '../../features/auth/authSlice';
 import styles from './Header.module.scss';
+import { AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai';
 
 function Header() {
 
@@ -24,22 +25,25 @@ function Header() {
                             <NavLink className={styles.header__navigation__logo} to='/'>e-Shop</NavLink>
                         </span>
                     </div>
-
                     <nav>
-                        <ul>
+                        <ul className={styles.header__navigation__links}>
                             <li>
+                                <AiOutlineShoppingCart className={styles.header__navigation__icon} />
                                 <NavLink className={styles.header__navigation__link} to='/cart'>Cart</NavLink>
-                                {user
-                                    ? (
-                                        <>
-                                            <NavLink className={styles.header__navigation__link} to='/profile'>Profile</NavLink>
-                                            <button className='btn' onClick={onLogout}>Logout</button>
-                                        </>
-                                    )
-                                    : (<NavLink className={styles.header__navigation__link} to='/sign-in'>Sign In</NavLink>)
-                                }
-
                             </li>
+                            {user
+                                ? (
+                                    <li>
+                                        <AiOutlineUser className={styles.header__navigation__icon} />
+                                        <NavLink className={styles.header__navigation__link} to='/profile'>Profile</NavLink>
+                                        <button className={`${styles['logout-btn']} btn`} onClick={onLogout}>Logout</button>
+                                    </li>
+                                )
+                                : (<li>
+                                    <AiOutlineUser className={styles.header__navigation__icon} />
+                                    <NavLink className={styles.header__navigation__link} to='/sign-in'>Sign In</NavLink> </li>)
+                            }
+
                         </ul>
                     </nav>
 
