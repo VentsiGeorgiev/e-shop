f82b614ef35c34923b53765c74776727c7d55a60
VentsiGeorgiev <ventsislavgeorgiev1@gmail.com>
2022-10-23 16:13:49 +0200

    Prepare for deploy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('./middleware/cors');
 const dotenv = require('dotenv').config();
+const path = require('path');
 const connectDB = require('./config/db');
 const { notFound, errorHandler } = require('./middleware/error');
 const PORT = process.env.PORT || 5000;
@@ -10,10 +11,6 @@ connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({ message: 'e-Shop API' });
-});
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -22,6 +19,20 @@ app.use(cors());
 app.use('/api/products', require('./routes/products'));
 app.use('/api/users', require('./routes/user'));
 app.use('/api/orders', require('./routes/order'));
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    app.get('*', (_, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+
+} else {
+    app.get('/', (req, res) => {
+        res.json({ message: 'e-Shop API' });
+    });
+}
 
 
 app.get('/api/config/paypal', (req, res) => res.json(process.env.PAYPAL_CLIENT_ID));
